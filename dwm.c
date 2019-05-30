300a547756645b59e63b5661c5d2ff627e953ff9
Ryan Geary <rtgnj42@gmail.com>
2019-05-30 09:59:32 -0400

    Add indicator bar

--- dwm.c
+++ dwm.c
@@ -723,7 +723,7 @@ drawbar(Monitor *m)
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, m->tagset[m->seltags] & 1 << i ? &scheme[SchemeSel] : &scheme[SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, tags[i], urg & 1 << i);
-		drw_rect(drw, x + 1, 1, dx, dx, m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
+		drw_rect(drw, x + dx, 0, w - ( 2 * dx + 1 ), dx / 2, m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 		           occ & 1 << i, urg & 1 << i);
 		x += w;
 	}
